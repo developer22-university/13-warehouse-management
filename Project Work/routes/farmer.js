72ca60a7e5397bc7c28464a63bc9fb65f666e6dd
Meet Shah <meet2410shah@gmail.com>
2021-04-13 11:33:51 +0530

    Farmer - get profile route added

--- Project Work/routes/farmer.js
+++ Project Work/routes/farmer.js
@@ -2,11 +2,20 @@
 const router = require('express').Router();
 
 // Controllers
-const { register, getProfile } = require('../controllers/farmer/');
+const {
+	register,
+	getProfile,
+	getWarehouses,
+	getCrops,
+	login,
+} = require('../controllers/farmer/');
 
 // Router Settings
 router.post('/register', register);
-router.get('/getProfile', getProfile);
+router.post('/login', login);
+router.post('/getProfile', getProfile);
+router.get('/getWarehouses', getWarehouses);
+router.post('/getCrops', getCrops);
 
 // Export the router
 module.exports = router;
